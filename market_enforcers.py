6dee9e5b9ca34862b4376e25a2bc573dd357ed3e
Teun van Wezel <vanwezelteun@gmail.com>
2026-08-15 17:27:45 +0200

    Add CIRProcess class to market_enforcers.py

--- market_enforcers.py
+++ market_enforcers.py
@@ -1,33 +1,51 @@
-from python_client.client import ExchangeClient, OrderInsert
-from python_client.exchange_types import Side, OrderType, AssetIdPair
 import time
 import random
 from math import sqrt 
+from dataclasses import dataclass
+
+from python_client.client import ExchangeClient, OrderInsert
+from python_client.exchange_types import Side, OrderType, AssetIdPair
 
 client = ExchangeClient("127.0.0.1")
 
+@dataclass
+class CIRProcess():
+    # Internal value
+    value: float
+    #Parameters
+    S_0: float
+    a: float
+    b: float
+    sigma: float
+
+    def __init__(self, S_0, a, b, sigma):
+        self.S_0 = S_0
+        self.a = a
+        self.b = b
+        self.sigma = sigma
+        self.value = S_0
+
+    def update(self, dt):
+        dWt = random.gauss(0, sqrt(dt))
+        self.value = self.value + self.a * (self.b - self.value) * dt + self.sigma * sqrt(self.value) * dWt
+
 # parameters
-S_0 = 0.85
-a = 0.01
-b = S_0
-sigma = 10.0
+cir_usd_eur = CIRProcess(0.85, 0.01, 0.85, 1.0)
+
 tps = 25
 volume_mean = 100
 
 
 rest = 1 / tps
-t = time.time()
-S = S_0
+start = time.time()
 order_id = 0
 
 while True:
     new_t = time.time()
-    dt_seconds = new_t - t
+    dt_seconds = new_t - start
     # 1 year = 256 trading days * 24 hours * 60 minutes * 60 seconds
     dt = dt_seconds / (60 * 60 * 24 * 256)
-
-    dWt = random.gauss(0, sqrt(dt))
-    S = S + a * (b - S) * dt + sigma * sqrt(S) * dWt
+    cir_usd_eur.update(dt)
 
     side = Side.Bid
     if order_id % 2 == 0:
@@ -38,7 +56,7 @@ while True:
         AssetIdPair(0, 1),
         side,
         int(random.expovariate(1/volume_mean)),
-        S
+        cir_usd_eur.value
     )
 
     client.send_commands([cmd])
